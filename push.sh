bf37131a529beac3e6534f139163bac231f48208
yyyr-otz <932624033@qq.com>
2023-12-12 00:50:41 +0800

    🚀 从windows工作笔记本推送 🕐 2023-12-12/00:50

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-git status 
+git status
 git add .
-git diff-index --quiet HEAD || (git commit -a -m "🚀 从windows工作笔记本推送 🕐 %date% " --allow-empty)
+git diff-index --quiet HEAD || (git commit -a -m "🚀 从windows工作笔记本推送 🕐 ` date +%F/%H:%M` " --allow-empty)
 git push
